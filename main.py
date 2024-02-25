9e84a2159addd0ef109ade6ac387b1b8fb49a771
Takeshi Sugiyama <kawacub@gmail.com>
2024-02-25 22:12:22 +0900

    bug fix

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ if len(st.session_state["words"]) == 0:
     df = pd.read_pickle("data/fivewords.pkl")
     st.session_state["words"] = df["word"]
     st.sidebar.text(f"{len(df)} data loaded.")
+words = st.session_state["words"]
 
 with st.form("input", clear_on_submit=True):
 
@@ -29,20 +30,17 @@ with st.form("input", clear_on_submit=True):
         if new_query not in st.session_state["query_list"]:
             st.session_state["query_list"].append(new_query)
 
-if len(st.session_state["words"]) < 500:
-    st.dataframe(st.session_state["words"], height=2000, hide_index=False)
-
 # サイドバー
 if len(st.session_state["query_list"]) > 0:
     for query in st.session_state["query_list"]:
-        st.session_state["words"] = word_check(st.session_state["words"], query)
-        st.sidebar.text(f'{query} : {str(len(st.session_state["words"]))}')
+        words = word_check(words, query)
+        st.sidebar.text(f'{query} : {str(len(words))}')
 
 if st.sidebar.button("クエリ削除"):
     st.session_state["query_list"] = []
-    st.reload()
     
 
+st.markdown(", ".join(words))
 
     
     
